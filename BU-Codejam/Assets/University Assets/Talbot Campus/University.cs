69dbf062de3cd954be8836e4c2e103b2f82c4e78
Kyle Russouw (s5606219) <s5606219@bournemouth.ac.uk>
2024-02-26 11:51:49 +0000

    University script created + initialised some variables

---
+++ BU-Codejam/Assets/University Assets/Talbot Campus/University.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class University : MonoBehaviour
+{
+    //initial class variables for what each building would need to include
+    private int Budget;
+    private int cost;
+    private float floodLevel;
+    private float studentSatisfaction;
+    private float energyConsumed;
+    private float waterConsumed;
+    private string buildingName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
