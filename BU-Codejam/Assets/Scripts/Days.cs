6dbbf7145107ff5776494ec3df0fb85164616d21
Kyle-Russ04 <kylejrussouw@gmail.com>
2024-02-26 16:48:42 +0000

    People

--- BU-Codejam/Assets/Scripts/Days.cs
+++ BU-Codejam/Assets/Scripts/Days.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using 
 
-public class Time : MonoBehaviour
+public class Days : MonoBehaviour
 {
     private int DayCounter;
     private int timePerDay = 10; 
