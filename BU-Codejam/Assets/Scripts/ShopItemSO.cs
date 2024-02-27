1283e6cbf264d09e42c40e9f647ac9f6e428acc3
Khadijah Ameliyah (s5607568) <s5607568@bournemouth.ac.uk>
2024-02-27 11:39:05 +0000

    fixed the shop ui

---
+++ BU-Codejam/Assets/Scripts/ShopItemSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShopMenu", menuName = "SciptableObjects/New Shop Item",order = 1)]
+public class ShopItemSO : ScriptableObject
+{
+    public string projectName;
+    public string description;
+    public int cost;
+}
--- BU-Codejam/Assets/Scripts/UpgradePurchase.cs
+++ BU-Codejam/Assets/Scripts/UpgradePurchase.cs
@@ -9,10 +9,13 @@ public class UpgradePurchase : MonoBehaviour
     private int LED_Light_Bulb_Replacement = 5000; //per building
     private int LED_Light_Fixture_Replacement = 85000; //per building
     //these two can only be one per building (not both)
+
     private int Lighting_Controls = 8000;
     private int Computer_Monitor_Upgrades = 200; //per monitor
+
     private int Monitor_Per_Building;
     //we can set that per building but just putting it here for now
+
     private int Pump_Upgrades = 40000; //per building
     private int IT_Server_Upgrades = 15000; //per building
     private int Boiler_Upgrades = 15000; //per building
@@ -22,7 +25,9 @@ public class UpgradePurchase : MonoBehaviour
     private int Air_source_heat_pump = 175000; //per building
     private int Heating_Vent_Cooling_Controls = 35000; //per building
     private int Glazing_Windows_Doors = 250000; //per building
+
     private int University_Wide_Campaign = 500; //each time
+
     //stuff that would reduce student satisfaction but not cost anything
     private int Heating_Lower;
     private int Change_Operate_Hours;
