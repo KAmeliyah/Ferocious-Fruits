35f2ed3c5b27be0510429ddb96e41c593027da16
Kyle Russouw (s5606219) <s5606219@bournemouth.ac.uk>
2024-02-27 15:04:36 +0000

    Kyle's Happiness

---
+++ BU-Codejam/Assets/Happiness.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Happiness : MonoBehaviour
+{
+    public Emotions scriptA;
+    private int happy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        happy = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (happy == 0) 
+        { 
+            //increase water levels
+        }
+    }
+}
