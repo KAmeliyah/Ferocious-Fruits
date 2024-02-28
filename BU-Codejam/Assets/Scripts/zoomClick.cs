b713858d47baa38e75b024cc752b3dd199ea6df7
Scott <scottlewwork@gmail.com>
2024-02-28 15:49:12 +0000

    added Start Screen

--- BU-Codejam/Assets/Scripts/zoomClick.cs
+++ BU-Codejam/Assets/Scripts/zoomClick.cs
@@ -35,7 +35,7 @@ public class zoomClick : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (camera.move == false && camera.focused == false)
+        if (camera.move == false && camera.focused == false && camera.lockMouse == false)
         {
             camera.changeTarget(lookAt.position);
             camera.focused = true;
